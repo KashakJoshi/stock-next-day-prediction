36c6d802202afb931ccc53d0d31ab6e50ffd2bf0
KashakJoshi <kashakjoshi777@gmail.com>
2026-04-01 02:35:01 +0530

    Docker file created

--- app.py
+++ app.py
@@ -2,29 +2,35 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-# 👉 IMPORT YOUR PIPELINE
+# 👉 your pipeline
 from prediction_engine.pipeline_run.run_pipeline import run_prediction_pipeline
 
 app = FastAPI()
 
-# HTML templates
 templates = Jinja2Templates(directory="templates")
 
-# ===== UI ROUTE =====
+
+# =========================
+# UI
+# =========================
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        request,
+        "index.html"
+    )
 
 
-# ===== API ROUTE =====
+# =========================
+# API (IMPORTANT)
+# =========================
 @app.get("/predict")
 def predict(ticker: str, date: str):
 
     try:
         result = run_prediction_pipeline(ticker, date)
 
-        # ✅ IMPORTANT FIX (no dict inside dict mistake)
-        return result  
+        return result
 
     except Exception as e:
         return {"error": str(e)}
